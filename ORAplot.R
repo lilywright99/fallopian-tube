7014e7e656b6eb1942f4c17ec61d3d7a4d8f18b6
lilywright99 <128158812+lilywright99@users.noreply.github.com>
2024-12-18 12:17:38 +0000

    Update ORAplot.R

--- ORAplot.R
+++ ORAplot.R
@@ -1,30 +1,24 @@
-library(dendextend)
-library(gplots)
-library(readr)
-library(magrittr)
-library(tidyverse)
+library(dplyr)
 library(ggplot2)
 library(ggrepel)
-library(ggdendro)
-library(reshape2)
+library(tidyverse)
 
-gwasstats<- read.csv("~/Documents/Fallopiantube/R/3NHSbroad_gwasranks.csv", header = TRUE)
-rankedgenes <- read.table("~/Documents/Fallopiantube/R/rowsumranksplot.csv", sep = ",", header=TRUE)
+ORA<-read.table("~/Documents/Fallopiantube/R/ORAresults.csv", sep=",", header = TRUE)
 
-gwasstats<- subset(gwasstats, gwasstats$gene %in% c(rankedgenes$fallopian.tube.gene, rankedgenes$luminal.gene, rankedgenes$glandular.gene))
-
-ggplot(gwasstats, aes(x = factor(gene, level=c('PGGHG','ANKRD36B','ANKRD36','SLC7A2','MUC1','CLDN1','HLA-DRB1','HLA-DRB5','PEX6','PLXNA4','NR2F1','GLS','CLGN')), y = Rank)) +
-  geom_point(aes(color = Hypernetwork), position = position_dodge(width = 0), size = 1.5) + 
+ggplot(ORA, aes(x = Tissue, y = Description)) +
+  geom_point(aes(size= -log10(FDR), color = -log10(P.Value))) +
+  scale_color_gradient(low = "blue", high = "red", name = "-log10(pValue)") +
+  scale_size_continuous(range = c(0.2, 15)) +
   theme_minimal() +
   labs(
-    title = "Ranks of Ectopic Pregnancy Associated Genes
-    in Fallopian tube and Endometrial Hypernetworks",
-    x = "Gene",
-    y = "Rank",
-    color = "Hypernetwork") +
+    title = "GeneOntology Over Representation Analysis 
+    for Fallopian Tube and Endometrial Secretory Epithelial Cells",
+    x = "Cell Types",
+    y = "Pathway Description" ) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        axis.title = element_text(size = 12),
-        plot.title = element_text(size = 14, hjust = 0.5))
+   plot.title = element_text(size = 14, face = "bold", hjust = 0.8))
+
+
 
 
 
